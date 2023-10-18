21c17021c20a56fff3a914c550978854036d3948
raykirie <lox@mail.ru>
2023-10-18 08:31:33 +0300

    comment

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function toggleDropdown(dropdownId) {
+    var dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+}
